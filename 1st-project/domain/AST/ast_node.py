b9a8a2d13504048dd74eadcb1d5e6c0856ecdcf8
Aylín Alvarez <aylinalvarezsantos@gmail.com>
2022-06-20 06:14:24 -0400

    Complete definitions of all classes shown in cypher.pdf

--- 1st-project/domain/AST/ast_node.py
+++ 1st-project/domain/AST/ast_node.py
@@ -1,15 +1,11 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import List,Tuple
+from distutils.util import change_root
+from tkinter import CHAR
+from typing import List,Tuple, Optional
 
 
 class AstNode(ABC):
-    pass
-
-class Return(AstNode):
-    pass
-
-class Where(AstNode):
     pass
 
 @dataclass
@@ -19,42 +15,87 @@ class Identifier: # Abstract
 class VarName(Identifier):
     pass
 
+class Literal(AstNode):
+    pass
+
 class NodeLabel(Identifier):
     pass
+
+@dataclass
+class Limit(AstNode):
+    number: int
 
 @dataclass
 class NodePattern(AstNode):
     variable:  VarName
     nodeLabels: List[NodeLabel]
-    properties: 
+    properties: List[Tuple[VarName, Literal]]
+
+@dataclass
+class AttributeAccessor(AstNode):
     pass
 
+@dataclass
+class Order:
+    ascending: bool
+    attribute: AttributeAccessor
+
+@dataclass
+class RelationDetails(AstNode):
+    details: List[Tuple[VarName, Literal]]
+    relationNames: List[Identifier]
+
+@dataclass
+class Projection(AstNode):
+    pass
+@dataclass
 class RelationPattern(AstNode):
+    rarrow: bool
+    larrow: bool
+    relationDetails: RelationDetails
+
+@dataclass
+class Pattern(AstNode):
+    node_pattern: NodePattern
+    relations: List[Tuple[RelationPattern, NodePattern]] 
+
+
+@dataclass
+class Atom(AstNode):
     pass
 
-class Pattern(AstNode):
-    node_pattern:NodePattern
-    relations:List[Tuple[RelationPattern, NodePattern]] 
+
+
+@dataclass
+class Comparison(AstNode):
+    left: Atom
+    right: Atom
+    operation: CHAR
+
+@dataclass
+class Conjunction(AstNode):
+    comparisons: List[Comparison]
+
+@dataclass
+class Condition(AstNode):
+    conjunctions: List[Conjunction]
+
+class Where(AstNode):
+    condition: Condition
 
 @dataclass
 class Match(AstNode):
-    optional:bool
-    pattern:Pattern
+    optional: bool
+    pattern: Pattern
     where_: Where 
-    
 
-
-
-
-
-
-
+class Return(AstNode):
+    distinct: Optional[bool]
+    limit: Optional[Limit]
+    skip: Optional[Condition]
+    projections: List[Projection]
 
 @dataclass
 class SingleQuery(AstNode):
     match: Match
     return_: Return
-
-
-
-
